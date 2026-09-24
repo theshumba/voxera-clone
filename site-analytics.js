a83cad1486cb7d7c2aaba14f7e2699159a393911
theshumba <ndoromelusi@gmail.com>
2026-09-24 02:13:49 +0100

    Connect LodgeHelm tracking to the verified EU analytics project

--- site-analytics.js
+++ site-analytics.js
@@ -1,4 +1,4 @@
-/* Shared free PostHog project. Megamind separates each business by website host. */
+/* Shared EU PostHog project 208594. Megamind separates each business by website host. */
 (() => {
   const host = location.hostname.replace(/^www\./, '');
   if (!['marsadesk.com', 'lodgehelm.com', 'sahemstrategy.com'].includes(host)) return;
@@ -24,7 +24,7 @@
   if (navigator.doNotTrack === '1' || navigator.globalPrivacyControl === true) return;
 
   !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split('.');2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement('script')).type='text/javascript',p.crossOrigin='anonymous',p.async=!0,p.src=s.api_host.replace('.i.posthog.com','-assets.i.posthog.com')+'/static/array.js',(r=t.getElementsByTagName('script')[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a='posthog',u.people=u.people||[],u.toString=function(t){var e='posthog';return'posthog'!==a&&(e+='.'+a),t||(e+=' (stub)'),e},u.people.toString=function(){return u.toString(1)+'.people (stub)'},o='init capture'.split(' '),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-  posthog.init('phc_F5sfdqn2hYZnrQVUKfoPb2uwOYkzgRo8drefABMmOnc', {
+  posthog.init('phc_xpgg4xbpp4mCYL2nVT7QUjYBCR34sckhqjiSYbpiDYy5', {
     api_host: 'https://eu.i.posthog.com',
     person_profiles: 'identified_only',
     capture_pageview: true,
